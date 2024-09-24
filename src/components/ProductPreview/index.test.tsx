4ee9b2a5513accd38c841cfb451cacb816af10ce
Iryna Antoniuk <antonjuk.iryna@gmail.com>
2024-09-24 12:53:05 +0300

    improved tests for ProductPreview

--- src/components/ProductPreview/index.test.tsx
+++ src/components/ProductPreview/index.test.tsx
@@ -1,81 +1,95 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { configureStore, Store } from '@reduxjs/toolkit';
+import { render, fireEvent } from '@testing-library/react';
+import { cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import ProductPreview from '.';
 import ActionsPanel from '../ActionsPanel';
-import { productsSlice, selectProduct } from '../../redux/slices/products';
-import { configureStore } from '@reduxjs/toolkit';
+import { productsSlice } from '../../redux/slices/products';
+import { RootState } from '../../redux/store';
+import { findByLabelTextType, getByRole } from '../../types';
 
-const mockStore = configureStore({
-  reducer: {
-    products: productsSlice.reducer,
-  },
+let store: Store<RootState>;
+
+beforeEach(() => {
+  store = configureStore({
+    reducer: {
+      products: productsSlice.reducer,
+    },
+  });
 });
 
-const store = mockStore;
+afterEach(cleanup);
+
+const renderWithStore = (component: React.ReactElement) =>
+  render(<Provider store={store}>{component}</Provider>);
+
+const getFormFields = async (findByLabelText: findByLabelTextType) => {
+  const nameInput = await findByLabelText('Name');
+  const descriptionTextArea = await findByLabelText('Description');
+  const priceInput = await findByLabelText('Price');
+
+  return { nameInput, descriptionTextArea, priceInput };
+};
+
+const clickAddButton = (getByRole: getByRole) => {
+  const addButton = getByRole('button', { name: '+ Add' });
+  fireEvent.click(addButton);
+};
 
 describe('ProductPreview', () => {
-  it('render placeholder', () => {
-    const store = mockStore;
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <ProductPreview />
-      </Provider>
-    );
+  it('should render the placeholder if no products selected', () => {
+    const { getByTestId } = renderWithStore(<ProductPreview />);
 
     const placeholder = getByTestId('preview-placeholder');
     expect(placeholder).toBeInTheDocument();
   });
 
-  it('add new product preview', async () => {
-    const { getByLabelText, getByRole, getByTestId } = render(
-      <Provider store={store}>
-        <React.Fragment>
-          <ActionsPanel />
-          <ProductPreview />
-        </React.Fragment>
-      </Provider>
+  it('should display form fields after adding a new product', async () => {
+    const { getByRole, findByLabelText, getByText } = renderWithStore(
+      <>
+        <ActionsPanel />
+        <ProductPreview />
+      </>
     );
 
-    const addButton = getByRole('button', { name: '+ Add' });
-    fireEvent.click(addButton);
-    store.dispatch(
-      selectProduct({
-        product: {
-          name: 'Product 1',
-          description: '',
-          price: 0,
-          id: 1,
-          creationDate: new Date(),
-        },
-        isNew: true,
-      })
-    );
+    clickAddButton(getByRole);
 
-    const nameInput = getByLabelText('Name');
-    const descriptionTextArea = getByLabelText('Description');
-    const priceInput = getByLabelText('Price');
-    const saveButton = getByRole('button', { name: 'Save' });
+    const { nameInput, descriptionTextArea, priceInput } =
+      await getFormFields(findByLabelText);
 
     expect(nameInput).toBeInTheDocument();
     expect(descriptionTextArea).toBeInTheDocument();
     expect(priceInput).toBeInTheDocument();
-    expect(saveButton).toBeInTheDocument();
-    expect(saveButton).toBeDisabled();
-    expect(() => getByTestId('preview-placeholder')).toThrow();
+    expect(
+      getByText(/name is required, only 30 symbols allowed/i)
+    ).toBeInTheDocument();
+    expect(
+      getByText(/price should be a number greater than 0/i)
+    ).toBeInTheDocument();
+
+    expect(store.getState().products.selectedProduct).toEqual({
+      id: expect.any(Number),
+      name: '',
+      description: '',
+      price: 0,
+      creationDate: expect.any(Date),
+    });
   });
 
-  it('messages for name and description', () => {
-    const { getByLabelText, getByText } = render(
-      <Provider store={store}>
+  it('should show validation messages for inputs', async () => {
+    const { getByRole, findByLabelText, getByText } = renderWithStore(
+      <>
+        <ActionsPanel />
         <ProductPreview />
-      </Provider>
+      </>
     );
 
-    const nameInput = getByLabelText('Name');
-    const descriptionTextArea = getByLabelText('Description');
-    const priceInput = getByLabelText('Price');
+    clickAddButton(getByRole);
+
+    const { nameInput, descriptionTextArea, priceInput } =
+      await getFormFields(findByLabelText);
 
     fireEvent.change(nameInput, { target: { value: 'a'.repeat(30) } });
     fireEvent.change(descriptionTextArea, {
@@ -92,16 +106,19 @@ describe('ProductPreview', () => {
     ).toBeInTheDocument();
   });
 
-  it('save product', () => {
-    const { getByLabelText, getByRole, getByTestId } = render(
-      <Provider store={store}>
+  it('save product', async () => {
+    const { getByRole, findByLabelText, getByTestId } = renderWithStore(
+      <>
+        <ActionsPanel />
         <ProductPreview />
-      </Provider>
+      </>
     );
 
-    const nameInput = getByLabelText('Name');
-    const descriptionTextArea = getByLabelText('Description');
-    const priceInput = getByLabelText('Price');
+    clickAddButton(getByRole);
+
+    const { nameInput, descriptionTextArea, priceInput } =
+      await getFormFields(findByLabelText);
+
     const saveButton = getByRole('button', { name: 'Save' });
 
     fireEvent.change(nameInput, { target: { value: 'a'.repeat(10) } });
@@ -115,5 +132,6 @@ describe('ProductPreview', () => {
 
     const placeholder = getByTestId('preview-placeholder');
     expect(placeholder).toBeInTheDocument();
+    expect(store.getState().products.selectedProduct).toBeNull();
   });
 });
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,12 @@
+import {
+  ByRoleMatcher,
+  ByRoleOptions,
+  Matcher,
+  SelectorMatcherOptions,
+  waitForOptions,
+} from '@testing-library/react';
+
+// app types
 export type Product = {
   id: number;
   name: string;
@@ -7,3 +16,15 @@ export type Product = {
 };
 
 export type SortOptions = 'name' | 'creationDate';
+
+// tests types
+export type findByLabelTextType = (
+  id: Matcher,
+  options?: SelectorMatcherOptions | undefined,
+  waitForElementOptions?: waitForOptions | undefined
+) => Promise<HTMLElement>;
+
+export type getByRole = (
+  role: ByRoleMatcher,
+  options?: ByRoleOptions | undefined
+) => HTMLElement;
